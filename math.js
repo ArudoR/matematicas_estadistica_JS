7d00a86d4f319db15d538e39f120bb3cd40780cb
Gerardo Mejia <aldonear@gmail.com>
2023-04-13 14:09:00 -0600

    funcion math de javascript explicada

--- math.js
+++ math.js
@@ -55,3 +55,33 @@ console.log({
 });
 
 console.groupEnd("Triangulo")
+
+//AQUÍ INICIAN LOS CALCULOS PARA EL CIRCULO
+
+console.group('Circle')
+const radioCirculo = 3
+const diametroCirculo = radioCirculo * 2
+const PI = 3.1415
+
+const circunferencia = diametroCirculo * PI
+const areaCirculo = (radioCirculo ** 2) * PI
+
+function calcularCirculo(radio) {
+    const diametro = radio * 2
+    const radioAlCuadrado = Math.pow(radio, 2)
+
+    return{
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI
+    }
+}
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo
+});
+
+console.groupEnd('Circle')
