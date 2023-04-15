c364ed7f0ef2cb192bec1bab0ef5525f678bca45
Gerardo Mejia <aldonear@gmail.com>
2023-04-15 01:23:13 -0600

    resolucion ejercicio altura de triangulo escaleno

--- math.js
+++ math.js
@@ -54,8 +54,6 @@ function calcularAlturaTriangulo(lado1, base) {
     }
 }
 
-//Reto: -Sabiendo lo que miden los 3 lados, encontrar altura de un triangulo escaleno (todos los lados diferentes).
-
 console.log({
     ladoTriangulo1,
     ladoTriangulo2,
@@ -66,6 +64,50 @@ console.log({
 });
 
 console.groupEnd("Triangulo")
+
+console.group('TrianguloEscaleno')
+//Reto: -Sabiendo lo que miden los 3 lados, encontrar altura de un triangulo escaleno (todos los lados diferentes).
+
+function calcularAlturaTrianguloEscaleno(lado1, lado2, lado3){
+    const semiPerimetro = (lado1 + lado2 + lado3) / 2
+
+    if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+        console.warn('Este no es un triangulo escaleno')
+    } 
+    const S = (lado1 + lado2 + lado3)/2
+    const S1 = S - lado1
+    const S2 = S - lado2
+    const S3 = S - lado3
+
+    const H =(2/lado1) * Math.sqrt(S * S1 * S2 * S3)
+    const result = Math.floor(H)
+
+    return result
+
+    
+    //     else{
+    //     altura = ((2/lado1) * (Math.sqrt(semiPerimetro * (semiPerimetro - lado1) * (semiPerimetro - lado2) * (semiPerimetro - lado3))))
+    //     return (altura)
+    // }
+}
+
+// export function solution(lado1, lado2, lado3) {
+//     if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+//       return false;
+//     }
+    
+//     const S = (lado1 + lado2 + lado3) / 2;
+//     const S1 = S - lado1;
+//     const S2 = S - lado2;
+//     const S3 = S - lado3;
+  
+//     const H = (2 / lado1) * Math.sqrt(S * S1 * S2 * S3);
+//     const result = Math.floor(H);
+  
+//     return result;
+//   }
+
+console.groupEnd('TrianguloEscaleno')
 
 //AQUÍ INICIAN LOS CALCULOS PARA EL CIRCULO
 
