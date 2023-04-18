ba29ce81294feea92e2ace7abf0b49034a1dad48
Aldo Mejia <aldonear@gmail.com>
2023-04-18 16:49:33 -0600

    metodo find vs metodo filter

--- descuentos.js
+++ descuentos.js
@@ -7,15 +7,17 @@ btn.addEventListener('click', calcularPrecioConDescuento)
 
 const arrayUObjeto = undefined //['Cupones': descuento]{}?
 
+const couponList = []
+    couponList.push({
+        name: 'ArudoR',
+        discount: 30,
+    })
+    couponList.push({
+        name: 'Descuent0',
+        discount: 25,
+    })
+
 function calcularPrecioConDescuento(){
-    // (P * (100-D)) / 100
-
-    const couponsObj = {
-        'ArudoR': 30,
-        'Descuent0': 25,
-        'Copymark': 15
-    }
-
     const price = Number(inputPrice.value)
     const coupon = inputCoupon.value
 
@@ -23,15 +25,43 @@ function calcularPrecioConDescuento(){
         pResult.innerText = `Por favor llena el formulario`
         return
     }
-
+    
     let discount
 
-    if (couponsObj[coupon]) {
-        discount = couponsObj[coupon]
-    } else {
-        pResult.innerText = 'El cupón no es válido'
+    // (P * (100-D)) / 100
+
+    // const couponsObj = {
+    //     'ArudoR': 30,
+    //     'Descuent0': 25,
+    //     'Copymark': 15
+    // }
+
+    function isCouponInArray(couponElement){ // {name, discount}
+       return couponElement.name == coupon
+    }
+
+    const couponInArray = couponList.find(isCouponInArray) //{}
+
+    if (couponInArray) {
+        discount = couponInArray.discount
+    } else{
+        pResult.innerText = 'El cupón no es valido'
         return
     }
+
+    console.log({
+        coupon,
+        discount,
+        couponInArray,
+        couponList
+    });
+
+    // if (couponsObj[coupon]) {
+    //     discount = couponsObj[coupon]
+    // } else {
+    //     pResult.innerText = 'El cupón no es válido'
+    //     return
+    // }
 
     // switch (coupon) {
     //     case 'ArudoR':
