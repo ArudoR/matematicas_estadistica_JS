9933337fb1cea03675fbf42a45f9b98af001dace
Aldo Mejia <aldonear@gmail.com>
2023-04-18 09:32:17 -0600

    ejercicio de descuento por medio de cupones en una funcion

--- descuentos.js
+++ descuentos.js
@@ -1,30 +1,65 @@
 const inputPrice = document.querySelector('#price')
-const inputDiscount = document.querySelector('#discount')
+const inputCoupon = document.querySelector('#coupon')
 const btn = document.querySelector('#calcular')
 const pResult = document.querySelector('#result')
 
 btn.addEventListener('click', calcularPrecioConDescuento)
 
+const arrayUObjeto = undefined //['Cupones': descuento]{}?
+
 function calcularPrecioConDescuento(){
     // (P * (100-D)) / 100
 
     const price = Number(inputPrice.value)
-    const discount = Number(inputDiscount.value)
+    const coupon = inputCoupon.value
 
-    console.log({price, discount});
-
-    if (!price || !discount) {
+    if (!price || !coupon) {
         pResult.innerText = `Por favor llena el formulario`
         return
     }
 
-    if(discount > 100){
-        pResult.innerText = `Ajá, ya quisieras, no te daremos billullo`
-        return
+    function calculateDiscount(coupon) {
+        const couponDiscounts = {
+            'ArudoR': 30,
+            'Descuent0': 25,
+        };
+
+        if (couponDiscounts.hasOwnProperty(coupon)) {
+            return couponDiscounts[coupon];
+        } else {
+            pResult.innerText = 'El cupón no es válido'
+            return
+        }
     }
+
+    const discount = calculateDiscount(coupon);
+
+    // switch (coupon) {
+    //     case 'ArudoR':
+    //         discount = 30;
+    //         break;
+        
+    //     case 'Descuent0':
+    //         discount = 25;
+    //         break;
+
+    //     default:
+    //         pResult.innerText = 'El cupón no es válido'
+    //         return
+    // }
+
+    // if (coupon == 'ArudoR') {
+    //     discount = 30;
+    // } else if (coupon == 'Descuent0') {
+    //     discount = 25;
+    // }else{
+    //     pResult.innerText = 'El cupón no es válido'
+    //     return
+    // }
+
+    console.log({price, discount});
 
     const newPrice = (price *(100 - discount))/100
 
     pResult.innerText = `El nuevo precio con descuento es: ${newPrice}`
-
 }
