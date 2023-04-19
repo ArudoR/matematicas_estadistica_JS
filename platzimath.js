a1606a185de5e3958ce7cb37d2cc3d8da22f9865
Aldo Mejia <aldonear@gmail.com>
2023-04-18 21:28:27 -0600

    metodo reduce explicado y uso de arrow functions

--- platzimath.js
+++ platzimath.js
@@ -1,3 +1,4 @@
+// [1,2,3,4]
 function calcularPromedio(lista){
     // let sumaLista = 0;
     // for (let i = 0; i < lista.length; i++) {
@@ -5,10 +6,14 @@ function calcularPromedio(lista){
     // }
 
     function sumarTodosElementos(valorAcumulado, nuevoValor){
-        return valorAcumulado + nuevoValor
+        return valorAcumulado + nuevoValor;
     }
+
+    // const sumarTodosElementos = (valorAcumulado, nuevoValor)=> valorAcumulado + nuevoValor;
     const sumaLista = lista.reduce(sumarTodosElementos);
-    
+
+    // const sumaLista = lista.reduce ((a, b) => a + b)
+
     const promedio = sumaLista / lista.length;
     console.log(promedio);
     return promedio;
