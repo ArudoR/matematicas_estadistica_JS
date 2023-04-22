017d390f775b14d71bd51ba366e5990955fea686
Gerardo Mejia <aldonear@gmail.com>
2023-04-21 23:32:43 -0600

    analisis salarial con JavaScript PT1

---
+++ analisis.js
@@ -0,0 +1,20 @@
+console.log(salarios);
+
+//Analisis personal para Juanita
+function encontrarPersona (personaEnBusqueda) {
+    return salarios.find(persona => persona.name == personaEnBusqueda)
+
+    // const persona = salarios.find((persona) => {
+    //     return persona.name = personaEnBusqueda
+    // });
+    // return persona;
+}
+
+function medianaPorPersona(nombrePersona){
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    const salarios = trabajos.map(function(elemento){
+        return elemento.salario;
+    })
+    console.log(salarios);
+}
